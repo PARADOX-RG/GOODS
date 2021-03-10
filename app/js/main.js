2d690ae143b3b788835d9b387a7c5372318c57c7
PARADOX-RG <vkurtik12@gmail.com>
2021-03-10 19:29:25 +0200

    added index and details

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+    $('.partners__list').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+    });
+
+
 });
