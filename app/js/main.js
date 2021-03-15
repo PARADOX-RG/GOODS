94f8d9cf94c13ff368fa8e51d7c4be44e3d200ea
PARADOX-RG <vkurtik12@gmail.com>
2021-03-15 12:52:14 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
-$(function(){
+
+
+
+$(function(){ 
+
+    $('.header__btn').on('click', function(){
+        $('.header__box').toggleClass('header__box--active')
+    });
+    $('.header__close').on('click', function(){
+        $('.header__box').removeClass('header__box--active')
+    });
 
     $('.partners__list').slick({
         slidesToShow: 5,
@@ -6,7 +16,30 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 1500,
         arrows: false,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1100,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            
+          ]
     });
 
+    
 
 });
